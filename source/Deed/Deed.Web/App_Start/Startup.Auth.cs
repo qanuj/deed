6c130de013094c39edb2b421f92d7a8386ec77f6
Anuj Pandey <a@e10.in>
2014-05-17 12:09:49 +0530

    Account Controller and Db

--- source/Deed/Deed.Web/App_Start/Startup.Auth.cs
+++ source/Deed/Deed.Web/App_Start/Startup.Auth.cs
@@ -13,38 +13,15 @@ namespace Deed.Web
 {
     public partial class Startup
     {
-        static Startup()
-        {
-            PublicClientId = "self";
-
-            UserManagerFactory = () => new UserManager<IdentityUser>(new UserStore<IdentityUser>());
-
-            OAuthOptions = new OAuthAuthorizationServerOptions
-            {
-                TokenEndpointPath = new PathString("/Token"),
-                Provider = new ApplicationOAuthProvider(PublicClientId, UserManagerFactory),
-                AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
-                AllowInsecureHttp = true
-            };
-        }
-
-        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
-
-        public static Func<UserManager<IdentityUser>> UserManagerFactory { get; set; }
-
-        public static string PublicClientId { get; private set; }
-
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
-        public void ConfigureAuth(IAppBuilder app)
-        {
+        public void ConfigureAuth(IAppBuilder app) {
             // Enable the application to use a cookie to store information for the signed in user
-            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-
-            // Enable the application to use bearer tokens to authenticate users
-            app.UseOAuthBearerTokens(OAuthOptions);
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
@@ -52,12 +29,12 @@ namespace Deed.Web
             //    clientSecret: "");
 
             //app.UseTwitterAuthentication(
-            //    consumerKey: "",
-            //    consumerSecret: "");
+            //   consumerKey: "",
+            //   consumerSecret: "");
 
             //app.UseFacebookAuthentication(
-            //    appId: "",
-            //    appSecret: "");
+            //   appId: "",
+            //   appSecret: "");
 
             //app.UseGoogleAuthentication();
         }
