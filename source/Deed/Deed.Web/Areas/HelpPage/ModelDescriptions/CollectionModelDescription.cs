a464aea06cf17cc5bd0fcf47b9a3fb51d85edd7b
viveke10 <vivek@e10.in>
2014-05-31 19:23:37 +0530

    conflicted files added

---
+++ source/Deed/Deed.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Deed.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
