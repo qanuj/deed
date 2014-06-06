91ccb5e1a909f38b64579811f465237d2ab7e30b
viveke10 <vivek@e10.in>
2014-06-06 18:28:28 +0530

    google map added

---
+++ source/Deed/Deed.Web/Controllers/ProjectsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Deed.Web.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        //
+        // GET: /Projects/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult SponsorChild()
+        {
+            return View();
+        }
+
+        public ActionResult SponsorProject()
+        {
+            return View();
+        }
+
+	}
+}
