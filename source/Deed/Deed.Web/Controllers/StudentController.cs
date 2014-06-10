c9c5fdce193b0a7dbf810df19ecca397829441f4
Anuj Pandey <a@e10.in>
2014-06-10 18:22:54 +0530

    Layout Modified

--- source/Deed/Deed.Web/Controllers/StudentController.cs
+++ source/Deed/Deed.Web/Controllers/StudentController.cs
@@ -13,8 +13,14 @@ namespace Deed.Web.Controllers
         readonly Deed.Data.Legacy.DataContext db = new Data.Legacy.DataContext();
 
         public ActionResult Query() {
-            var students=db.Students.OrderByDescending(x=>x.id).Select(x=>new { last=x.last_name,picture=x.image,gender=x.male }).Take(10);
+            var students=db.Students.OrderByDescending(x=>x.id).Select(x=>new
+            { last=x.last_name,picture=x.image,gender=x.male }).Take(10);
             return Json(students, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Videos() {
+            var items = db.YouTubeVideos.OrderByDescending(x => x.id).Take(10);
+            return Json(items, JsonRequestBehavior.AllowGet);
         }
 	}
 }
