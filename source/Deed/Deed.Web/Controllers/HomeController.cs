808e49bbcdcf5eb567af62aa71d077ac496dac16
viveke10 <vivek@e10.in>
2014-07-10 15:44:13 +0530

    success stories added

--- source/Deed/Deed.Web/Controllers/HomeController.cs
+++ source/Deed/Deed.Web/Controllers/HomeController.cs
@@ -104,73 +104,26 @@ namespace Deed.Web.Controllers {
             return result;
         }
 
+        public ActionResult Story()
+        {
+            var story = from s in db.Stories
+
+                    select new StoryViewModel
+                    {
+                       StoryId=s.id,
+                       StoryTitle=s.title,
+                       StoryImage=s.image,
+                       StoryContent=s.content,
+                       CreatedOn=s.created,
+                       CreatedBy=s.created_by
+                    };
+            //story = story.OrderBy(x => x.CreatedOn);
+            return PartialView("_story", story);
+        }
+
     }
 }
-//        public ActionResult Search(string StudentName, string Religion, string Country, string State)
-//        {
-//            if (!String.IsNullOrEmpty(StudentName))
-//            {
-//                var Re = Convert.ToInt64(Religion);
-//                var Co = Convert.ToInt64(Country);
-//                var St = Convert.ToInt64(State);
-//                var s = from m in db.Students select m;
-//                s = s.Where(c => c.Name == StudentName || c.Religion.ID == Re || c.Country.ID == Co || c.State.ID == St);
-//                return View("SearchResult", s);
-//            }
-//            return RedirectToAction("Index");
-//        }
 
 
-//        public ActionResult StudentDetails(long id)
-//        {
-//            var s = db.Students.FirstOrDefault(x => x.ID == id);
-//            return View("StudentDetails", s);
 
-//        }
-
-//        public ActionResult Index() 
-//        {
-//            ViewBag.Title = "Home Page";
-//            ViewBag.Religion = new SelectList(GetReligion(), "ID", "Name");
-//            ViewBag.Country = new SelectList(GetCountry(),"ID","Name");
-//            ViewBag.State = new SelectList(GetState(), "ID", "Name");
-//            ViewBag.City = new SelectList(GetCity(), "ID", "Name");
-//            return View();
-//        }
-
-//        public ICollection<ComboItem> GetReligion(string firstItem = "", long? selected = 0)
-//        {
-//            var r = from c in db.Students select new ComboItem { ID = c.Religion.ID, Name = c.Religion.Name };
-//            var result = r.Distinct().ToList();
-//            return result;
-//        }
-
-//        public ICollection<ComboItem> GetCountry(string firstItem = "", long? selected = 0)
-//        {
-//            var r = from c in db.Students select new ComboItem { ID = c.Country.ID, Name = c.Country.Name };
-//            var result = r.Distinct().ToList();
-//            return result;
-//        }
-
-//        public ICollection<ComboItem> GetState(string firstItem = "", long? selected = 0)
-//        {
-//            var r = from c in db.Students select new ComboItem { ID = c.State.ID, Name = c.State.Name };
-//            var result = r.Distinct().ToList();
-//            return result;
-//        }
-//        public ICollection<ComboItem> GetCity(string firstItem = "", long? selected = 0)
-//        {
-//            var r = from c in db.Students select new ComboItem { ID = c.City.ID, Name = c.City.Name };
-//            var result = r.Distinct().ToList();
-//            return result;
-//        }
-
-//        //public ActionResult Index()
-//        //{
-            
-//        //    return View();
-//        //}
-
-//    }
-//}
 
---
+++ source/Deed/Deed.Web/ViewModels/StoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Deed.Web.ViewModels
+{
+    public class StoryViewModel
+    {
+
+        public long  StoryId { get; set; }
+        public string  StoryTitle { get; set; }
+        public string StoryImage { get; set; }
+        public string  StoryContent { get; set; }
+        public DateTime  CreatedOn { get; set; }
+        public long  CreatedBy { get; set; }
+
+    }
+}
